819aeacee95699a692643701e465a0b3489b9d35
rimuru2003 <karansnh007@gmail.com>
2024-06-30 15:23:05 +0530

    portfolio

--- task1/script.js
+++ task1/script.js
@@ -1,36 +1,6 @@
-var header = document.getElementById("header");
-var mc = document.getElementById("Pani");
-var m3 = document.getElementById("Ice");
-var mx = document.getElementById("Vada");
-var my = document.getElementById("Dabeli");
-var model = document.getElementById("price"); // Changed variable name from 'model' to 'price'
-var mph = document.getElementById("desc"); // Changed variable name from 'mph' to 'desc'
-
-mc.onclick = function () {
-  header.style.backgroundImage =
-    "url('asset/pani_puri_90458_16x9.jpg')";
-
-  price.innerHTML = "Pani Puri";
-  desc.innerHTML = "$2.5";
-};
-
-m3.onclick = function () {
-    header.style.backgroundImage =
-    "url('asset/american-heritage-chocolate-PWwKJLHzekk-unsplash.jpg')";
-  price.innerHTML = "Ice Cream";
-  desc.innerHTML = "$3.5";
-};
-
-mx.onclick = function () {
-    header.style.backgroundImage =
-    "url('asset/shakti-rajpurohit-73L6wSNLpSk-unsplash.jpg')";
-  price.innerHTML = "Pav Bhaji";
-  desc.innerHTML = "$1.5";
-};
-
-my.onclick = function () {
-    header.style.backgroundImage =
-    "url('asset/anil-sharma-S6WyKp5jdkQ-unsplash.jpg')";
-  price.innerHTML = "Dosa";
-  desc.innerHTML = "$2.0";
-};
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
